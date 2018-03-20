8b66f4ce676d7d583f1c8b62b5536774fcf9a090
Rendi Wahyudi Muliawan <rendy1287@gmail.com>
2018-03-20 15:40:24 +0700

    Update beberapa baris script (belum selesai)

--- tokopedia.user.js
+++ tokopedia.user.js
@@ -3,14 +3,13 @@
 // @author       Rendi Wahyudi Muliawan (Celleven Store)
 // @namespace    http://www.tokopedia.com/celleven
 // @source       https://github.com/rendy1287/tokopedia-cetak-sekaligus
-// @version      0.01
+// @version      0.03
 // @description  Untuk mencetak label alamat pada Tokopedia bagi pengguna Non / Bukan Gold Merchant.
 // @license      Anda diperbolehkan menyalin dan mengedit script ini, tapi mohon cantumkan author dan website kami.
 // @match        https://www.tokopedia.com/myshop_order_process.pl*
 // @match        https://tokopedia.com/myshop_order_process.pl*
 // @include      https://www.tokopedia.com/myshop_order_process.pl*
 // @include      https://tokopedia.com/myshop_order_process.pl*
-// @require      https://code.jquery.com/jquery-3.3.1.min.js
 // @run-at       document-end
 // ==/UserScript==
 
@@ -36,24 +35,28 @@
 //                isikan url image kamu, bisa upload ke image hosting
 //                logo recommended size: 115x25 pixel
 //
-// @logoekspedisi menampilkan logo ekspedisi
-//                isikan true jika ingin menampilkan atau false jika tidak
+// @ekspedisi     menampilkan logo ekspedisi, isikan true jika
+//                ingin menampilkan atau false jika tidak
 //
-// @invoice       menampilkan nomor invoice
-//                isikan true jika ingin menampilkan atau false jika tidak
+// @invoice       menampilkan nomor invoice, isikan true jika
+//                ingin menampilkan atau false jika tidak
 //
 // @fontsize      ukuran font untuk label
 //
 //******************************************************************//
 
-var logokamu = '';
-var logoekspedisi = 0;
-var invoice = 0;
-var fontsize = '15px';
+var logokamu  = 'http://www.domainkamu.com/logo.jpg';
+var ekspedisi = false;
+var invoice   = false;
+var fontsize  = '15px';
 
 //*****************************************************************//
+//
+//  SCRIPT DI BAWAH INI JANGAN DIUBAH JIKA TIDAK MENGERTI
+//
+//*****************************************************************//
 
-var btncetak, cetak, radio = [];
+var btncetak, cetak;
 
 (function() {
     'use strict';
@@ -77,9 +80,7 @@ var btncetak, cetak, radio = [];
 function cetak_alamat()
 {
     $('html').empty();
-    $('html').html('<head><title>Judul</title></head><body>Di sini untuk mencetak alamat</body>');
-
-    console.log('cetak');
+    $('html').html('<head><title>Cetak Slip Alamat</title></head><body>Di sini untuk mencetak alamat</body>');
 
 }
 
@@ -89,7 +90,7 @@ function cetak_button()
     btncetak  = '<style>button.t-c-s{background-color: #3a87ad; border: 1px solid #3a87ad; color: white;}' +
                 'button.t-c-s:hover{background-color: #3a679d; border: 1px solid #3a679d; color: white;}</style>' +
                 '<button class="btn btn-small mr-5 t-c-s">' +
-                '<i class="icon-fax-alt"></i> Cetak Sekaligus</button>';
+                '<i class="icon-fax-alt"></i> ' + ($('html').attr('lang') == 'id' ? 'Cetak Sekaligus' : 'Print Multiple') + '</button>';
 
     $(btncetak).insertAfter('button.confirm-multiple');
 
